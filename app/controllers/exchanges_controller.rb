24115adff2d7ad5aa829d240821d85baf00265e8
Armando <atdc.codemaster@gmail.com>
2020-09-01 14:45:28 -0300

    pass exchanges_request_spec

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -1,7 +1,19 @@
 class ExchangesController < ApplicationController
+  before_action :set_params, only: [:convert]
+
   def index
   end
 
   def convert
+    value = ExchangeService.new(@source_currency, @target_currency, @amount).call
+    render json: {value: value}
   end
+
+  private
+
+    def set_params
+      @source_currency = params[:source_currency]
+      @target_currency = params[:target_currency]
+      @amount = params[:amount]
+    end
 end
--- spec/requests/exchanges_request_spec.rb
+++ spec/requests/exchanges_request_spec.rb
@@ -10,13 +10,20 @@ RSpec.describe "Exchanges", type: :request do
   end
 
   describe "GET /convert" do
-    it "returns http success" do
+    it "returns correct converted value" do
+      amount = rand(1..9999)
+      source_currency = "USD"
+      target_currency = "BRL"
+      value = ExchangeService.new(source_currency, target_currency, amount).call
+
       get "/convert", params: {
         source_currency: "USD",
         target_currency: "BRL",
-        amount: rand(1..9999)
+        amount: amount
       }
       expect(response).to have_http_status(:success)
+      expect(response.content_type).to include('application/json')
+      expect(JSON.parse(response.body)['value']).to eq(value)
     end
   end
 
