3363bdbb0d544da5f3b77e8639ff92f156d5d20c
Armando <atdc.codemaster@gmail.com>
2020-09-01 23:01:10 -0300

    improve user experience

--- app/javascript/packs/exchanges.js
+++ app/javascript/packs/exchanges.js
@@ -1,6 +1,33 @@
 document.addEventListener('turbolinks:load', function () {
+  let resultElement = document.getElementById('result')
+  let amountElement = document.getElementById('amount')
+
   document.getElementById('exchange_form').addEventListener('ajax:success', function (event) {
     let [result] = event.detail
-    document.getElementById('result').value = result.value
+    resultElement.value = result.value
+    amountElement.readOnly = false
   })
+
+  let cleanForm = () => {
+    resultElement.value = ""
+    amountElement.value = ""
+  }
+
+  let handleConvert = () => {
+    amountElement = amountElement
+    submitButton = document.getElementById('submit-button')
+
+    if (amountElement.value > 0) {
+      submitButton.click()
+      amountElement.readOnly = true
+    } else {
+      resultElement.value = ""
+    }
+  }
+
+  amountElement.onkeyup = handleConvert
+  amountElement.onchange = handleConvert
+
+  document.getElementById('target_currency').onchange = cleanForm
+  document.getElementById('source_currency').onchange = cleanForm
 })
